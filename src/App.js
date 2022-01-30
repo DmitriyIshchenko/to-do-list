cf6a122ee65b97fed445196f4e98c99ea63bca80
Dmitry Ishchenko <to.dmitry.ishchenko@gmail.com>
2022-01-30 12:08:32 +0300

    Setup routing

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import React from 'react';
 import './App.css';
+import { Routes, Route } from "react-router-dom";
+import Lists from './features/todo/Lists';
+import CreateList from './features/todo/CreateListForm';
+import SingleList from './features/todo/SingleList';
 
 function App() {
   return (
     <div className="App">
-      init
+      <Routes>
+        <Route path="/" element={<Lists />} />
+        <Route path="/create-list" element={<CreateList />} />
+        <Route path="/list/:listId" element={<SingleList />} />
+      </Routes>
     </div>
   );
 }
