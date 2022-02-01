e0d9a3fc73ca25bd868066b812210d4c0159d56e
Dmitry Ishchenko <to.dmitry.ishchenko@gmail.com>
2022-02-01 16:15:03 +0300

    Style single task component

--- src/features/todo/SingleTask.js
+++ src/features/todo/SingleTask.js
@@ -3,6 +3,9 @@ import { useDispatch } from 'react-redux';
 import { toggleTaskStatus, deleteTask } from "./todoSlice";
 import { Link } from "react-router-dom";
 
+import "../../styles/SingleTask.css"
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+
 export default function SingleTask({ todo, listId }) {
 
     const dispatch = useDispatch();
@@ -16,10 +19,21 @@ export default function SingleTask({ todo, listId }) {
         dispatch(deleteTask({ listId, taskId }))
     }
 
-    return <>
-        <input type="checkbox" checked={isDone} onChange={handleToggleStatus} />
-        {task}
-        <button onClick={handleDelete}>delete</button>
-        <Link to={`/list/${listId}/${taskId}/edit`}>edit</Link>
-    </>;
+    return <li className='single-task'>
+
+        <label className='single-task-label'>
+            <input type="checkbox" checked={isDone} onChange={handleToggleStatus} /><p>{task}</p>
+            <span className='checkmark'></span>
+        </label>
+
+        <div>
+            <button className="delete-btn" onClick={handleDelete}>
+                <FontAwesomeIcon icon="trash-alt" />
+            </button>
+            <Link className="edit-btn" to={`/list/${listId}/${taskId}/edit`}>
+                <FontAwesomeIcon icon="pen" />
+            </Link>
+        </div>
+
+    </li >;
 }
