20ffa9a1d013916fbf7cee7eabd162ceb81602e0
Dmitry Ishchenko <to.dmitry.ishchenko@gmail.com>
2022-02-02 16:33:07 +0300

    Style and add icon dropdown

--- src/features/todo/CreateListForm.js
+++ src/features/todo/CreateListForm.js
@@ -1,7 +1,10 @@
 import React, { useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { useNavigate } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import { createList, selectAllLists } from './todoSlice';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+
+import "../../styles/CreateListForm.css"
 
 export default function CreateList() {
 
@@ -12,19 +15,66 @@ export default function CreateList() {
 
     const [title, setTitle] = useState("");
     const [colorTheme, setColorTheme] = useState("#0000FF");
+    const [icon, setIcon] = useState("home");
+    const [showDropdown, setShowDropdown] = useState(false)
+
     const canSave = lists.every(item => item.title !== title) && title;
 
     const handleCreate = () => {
-        const response = dispatch(createList(title, colorTheme));
+        const response = dispatch(createList(title, colorTheme, icon));
         navigate(`/list/${response.payload.listId}`);
+        setShowDropdown(false);
     }
 
-    return <section>
-        <h2>Create a list</h2>
-        <form>
-            <input type="text" value={title} onChange={(e) => setTitle(e.target.value)} />
-            <input type="color" value={colorTheme} onChange={(e) => setColorTheme(e.target.value)} />
-            <button type="button" onClick={handleCreate} disabled={!canSave}>create</button>
-        </form>
-    </section>;
+    const icons = [
+        "graduation-cap",
+        "briefcase",
+        "home",
+        "dumbbell",
+        "book",
+        "hammer",
+        "shopping-cart",
+        "birthday-cake",
+        "box-open",
+        "capsules",
+        "dog",
+        "laptop",
+        "palette",
+        "music"
+    ]
+
+    const iconDropdownContent = icons.map((icon, index) => {
+        return <FontAwesomeIcon key={index} onClick={() => setIcon(icon)} icon={icon} style={{ color: colorTheme }} />
+    })
+
+    return <form className='create-list-form'>
+        <Link to="/" className='link-back'><FontAwesomeIcon icon="angle-left" /></Link>
+
+        <header>
+            <h1>Create new list</h1>
+        </header>
+
+        <input type="text" placeholder="Title" value={title} onChange={(e) => setTitle(e.target.value)} />
+
+        <div className='options'>
+
+            <button type="button" className="icon-dropbtn" onClick={() => setShowDropdown(!showDropdown)}>
+                <FontAwesomeIcon icon={icon} style={{ color: colorTheme }} />
+                <span style={{ transform: `rotate(${showDropdown ? 45 : -45}deg)` }}></span>
+            </button>
+            <div className={showDropdown ? "icon-dropdown show-dropdown" : "icon-dropdown"}>
+                {iconDropdownContent}
+            </div>
+
+            <div className='color-picker'>
+                <input type="color" value={colorTheme} onChange={(e) => setColorTheme(e.target.value)} />
+            </div>
+
+        </div>
+
+        <button className='save-btn' type="button" onClick={handleCreate} disabled={!canSave}>
+            <FontAwesomeIcon icon="plus" /> add list
+        </button>
+
+    </form>;
 }
